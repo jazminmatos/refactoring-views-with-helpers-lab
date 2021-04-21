1298e18378668910a430bd45b5b59b0ba96e08d8
jazminmatos <jmatos1993@gmail.com>
2021-04-20 23:21:25 -0400

    commit tree 127a3ce412633d3e7cc70ff01ce1a116d2aadf8b

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song && song.artist
+            #return a link to the artist's show page
+            link_to song.artist_name, artist_path(song.artist)
+        else
+            #return a link to the song's edit page, with a link text of "Add Artist"
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
